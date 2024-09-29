8e75401b26c15943b6c909425a8feb9886051d6d
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-09-29 17:16:38 +0800

    Defined the schema for Items

---
+++ backend/models/Items.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const newItem = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    account: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Item',
+        required: true
+    }
+})
+
+newItem.set('toJSON', {
+    transform: function (doc, ret, options) {
+        delete ret.__v;
+        delete ret.updatedAt;
+        return ret;
+    }
+});
+
+const Item = mongoose.model('Item', newItem)
+
+export default Item;
