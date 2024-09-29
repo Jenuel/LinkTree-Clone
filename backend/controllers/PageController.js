b712d89022994b6554fd2ef4221983cde1ac6950
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-09-29 13:59:25 +0800

    Initialized the controller

---
+++ backend/controllers/PageController.js
@@ -0,0 +1,15 @@
+import { request } from "express";
+
+const authenticate = async (request, response) => {
+
+}
+
+const addLink = async (request, response) => {
+
+}
+
+const removeLink = async (request, response) => {
+
+}
+
+export { authenticate, addLink, removeLink }
