8b6912ebb57adce7c4b9635c4c95445cfcdcada5
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-09-29 17:51:30 +0800

    Coded the impementation for the getLinks

--- backend/controllers/PageController.js
+++ backend/controllers/PageController.js
@@ -1,19 +1,29 @@
 import { request } from "express";
+import Item from '../models/Items';
 
-const authenticate = async (request, response) => {
-
-}
-
-const register = async (request, response) => {
-
-}
-
+//Create Read Delete
 const addLink = async (request, response) => {
 
+}
+
+const getLinks = async (request, response) => {
+    const { id } = request.params
+
+    try {
+        const items = await Item.find({ account: id}).sort({ createdAt: -1 })
+
+        if (!items || items.length === 0) {
+            return response.status(404).send({ message: 'There are no items associated with this account' })
+        }
+
+        response.status(200).json(items)
+    } catch (error) {
+        response.status(500).send({ message: 'Error fetching data', error: error.message })
+    }
 }
 
 const removeLink = async (request, response) => {
 
 }
 
-export { authenticate, register, addLink, removeLink }
+export { addLink, getLinks, removeLink }
