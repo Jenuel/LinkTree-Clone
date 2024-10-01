bec42bb868ebc11f6e2cc49307c936d7b6e910d3
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-10-01 22:31:51 +0800

    Created Jwt

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -2,12 +2,23 @@ import User from "../models/User"
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
+
+const maxAge = 4 * 24 * 60 *60
+
+const createJWT = (id) => {
+    return jwt.sign({ id }, 'S3CR3T', {
+        expiresIn: maxAge
+    });
+}
+
 const register = async (request, response) => {
     const { firstName, lastName, username, password } = request.body;
 
     try {
         const user = User.create({ firstName, lastName, username, password });
-        response.status(201).json(user)
+        const token = createJWT(user._id)
+        response.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000})
+        response.status(201).json(user._id)
     } catch (error) {
         response.status(400).send('Please send appropriate data! User not registered.')
     }
