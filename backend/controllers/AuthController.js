84dfb604ce622d49f8e5431c60b55aa9e6034d63
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-10-01 18:29:27 +0800

    Initialize the controller for authentication

---
+++ backend/controllers/AuthController.js
@@ -0,0 +1,20 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const register = async (request, response) => {
+
+}
+
+const login = (request, response) => {
+
+}
+
+const refresh = (request, response) => {
+
+}
+
+const logout = (request, response) => {
+
+} 
+
+export { register, login, refresh, logout }
