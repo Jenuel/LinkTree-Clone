db0ea99bff9235461c16332bd91752ed3a7e8ab6
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-10-01 18:31:42 +0800

    Updated the register function

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,8 +1,16 @@
+import User from "../models/User"
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 const register = async (request, response) => {
+    const { firstName, lastName, username, password } = request.body;
 
+    try {
+        const user = User.create({ firstName, lastName, username, password });
+        response.status(201).json(user)
+    } catch (error) {
+        response.status(400).send('Please send appropriate data! User not registered.')
+    }
 }
 
 const login = (request, response) => {
