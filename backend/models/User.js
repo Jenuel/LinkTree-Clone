b4cf896fcbe55254e500f757044fa035b978b22f
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-10-02 21:56:11 +0800

    Add a function for verifying the credentials

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcrypt';
 
 const Schema = mongoose.Schema;
 
-const newUser = new Schema({
+const userSchema = new Schema({
     firstName: {
         type: String,
         required: true
@@ -22,7 +22,7 @@ const newUser = new Schema({
     }
 })
 
-newUser.set('toJSON', {
+userSchema.set('toJSON', {
     transform: function (doc, ret, options) {
         delete ret.__v;
         delete ret.updatedAt;
@@ -30,12 +30,24 @@ newUser.set('toJSON', {
     }
 });
 
-newUser.pre('save', async function (next) {
+userSchema.pre('save', async function (next) {
     const salt = await bcrypt.genSalt()
     this.password = await bcrypt.hash(this.password, salt);
     next();
 })
 
-const User = mongoose.model('User', newUser)
+userSchema.statics.login = async function(username, password){
+    const user = await this.findOne({ username })
+    if (user) {
+        const result = await bcrypt.compare(password, user.password)
+        if (result) {
+            return user;
+        }
+        throw Error("Incorrect password or email")
+    }
+    throw Error("Incorrect password or email")
+}
+
+const User = mongoose.model('User', userSchema)
 
 export default User;
