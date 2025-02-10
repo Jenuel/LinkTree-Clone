4b3751a74b6ee898b5c3b9dac44dcde95cd11852
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2025-02-10 18:44:32 +0800

    added urls

--- frontend/src/app/admin/AdminDashboard.js
+++ frontend/src/app/admin/AdminDashboard.js
@@ -60,6 +60,7 @@ export default function AdminDashboard({ initialData }) {
                         <TableRow key={item.id}>
                             <TableCell>{item.id}</TableCell>
                             <TableCell>{item.name}</TableCell>
+                            <TableCell>{item.url}</TableCell>
                             <TableCell>
                                 <Button className="mr-2" onClick={() => deleteItem(item.id)}>Delete</Button>
                                 <Dialog>
