0be739058aa976e93285f4afe49c8625676b333b
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2025-02-17 15:21:03 +0800

    Configured the dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import cookieParser from 'cookie-parser'
-import PageRoutes from "./routes/PageRoutes";
-const app = express()
+import PageRoutes from "./routes/PageRoutes.js";
+import dotenv from 'dotenv';
 
+const app = express()
+dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", PageRoutes)
